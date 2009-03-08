a6a0a1003b19e66afdc62c7f0cfb411c309200da
Jason Harrelson <jason@lookforwardenterprises.com>
2009-03-07 23:24:40 -0600

    Added view helper.

--- lib/flash_growler.rb
+++ lib/flash_growler.rb
@@ -1,6 +1,10 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'flash_growler/view_helpers'
+
 module FlashGrowler
   VERSION = '0.0.1'
 end
+
+ActionView::Base.send( :include, FlashGrowler::ViewHelpers ) if defined?( ActionView )
---
+++ lib/flash_growler/view_helpers.rb
@@ -0,0 +1,27 @@
+module FlashGrowler
+  module ViewHelpers
+    
+    def g_flash_growler( *args )
+      return '' if flash.nil? || flash.empty?
+      options = args.extract_options!
+      #raise ArgumentError, "'cols' option required" unless options.include?( :cols )
+      
+      position_cls = ''
+      if options[:position] == 'top'
+        position_cls = 'growl-top'
+      else
+        position_cls = 'growl-bottom'
+      end
+      
+      add_to_inits( :flash_growler, options, [ 'jquery/jquery-growler-0.1.js' ] )
+      if flash[:error]
+        return "<div class=\"growl flash error #{position_cls}\" id=\"#{options[:id]}\"><div id=\"growl-icon-error\"></div><h3>Error</h3><p>#{flash[:error]}</p></div>"
+      elsif flash[:warning]
+        return "<div class=\"growl flash warning #{position_cls}\" id=\"#{options[:id]}\"><div id=\"growl-icon-alert\"></div><h3>Warning</h3><p>#{flash[:warning]}</p></div>"
+      elsif flash[:notice]
+        return "<div class=\"growl flash notice #{position_cls}\" id=\"#{options[:id]}\"><div id=\"growl-icon-msg\"></div><h3>Info</h3><p>#{flash[:notice]}</p></div>"
+      end
+    end
+    
+  end
+end
