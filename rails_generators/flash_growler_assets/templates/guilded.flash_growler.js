cee54b8d9509784cf88e3a9886f6740273571486
Jason Harrelson <jason@lookforwardenterprises.com>
2009-03-09 20:26:03 -0500

    Added before and after init callbacks.

--- rails_generators/flash_growler_assets/templates/guilded.flash_growler.js
+++ rails_generators/flash_growler_assets/templates/guilded.flash_growler.js
@@ -1,4 +1,10 @@
 g.flashGrowlerInit = function( options ) 
 {
+	if( g.beforeLiveValidatorInit )
+    g.beforeFlashGrowlerInit( options );
+
   $j( '#' + options.id ).growl( options );
+
+	if( g.afterLiveValidatorInit )
+    g.afterFlashGrowlerInit( options );
 };
