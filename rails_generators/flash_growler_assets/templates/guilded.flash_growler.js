d187e29288ae9af6c11fcecafdde83315411120c
Jason Harrelson <jason@lookforwardenterprises.com>
2009-03-11 20:51:16 -0500

    Release 1.0.5.

--- rails_generators/flash_growler_assets/templates/guilded.flash_growler.js
+++ rails_generators/flash_growler_assets/templates/guilded.flash_growler.js
@@ -1,4 +1,4 @@
-/* Guilded Flash Growler 1.0.4
+/* Guilded Flash Growler 1.0.5
  * Copyright (c) 2009 C. Jason Harrelson (midas)
  * Guilded Flash Growler is licensed under the terms of the MIT License */
 
