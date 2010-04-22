7a290c688323c04ad31d725c1284f23f18b8f5ba
Jason Harrelson <jason@lookforwardenterprises.com>
2010-04-22 14:59:13 -0500

    Added a mapping mechanism for differntly named flash messages.

--- lib/g_flash_growler/view_helpers.rb
+++ lib/g_flash_growler/view_helpers.rb
@@ -4,16 +4,18 @@ module GFlashGrowler
     def g_flash_growler( *args )
       return '' if flash.nil? || flash.empty?
       options = args.extract_options!
-      #raise ArgumentError, "'cols' option required" unless options.include?( :cols )
+      mappings = options[:mappings] || {}
       
-      position_cls = ''
-      if options[:position] == 'top'
-        position_cls = 'growl-top'
-      else
-        position_cls = 'growl-bottom'
-      end
+      position_cls = options[:position] == 'top' ? 'growl-top' : 'growl-bottom'
       
       Guilded::Guilder.instance.add( :flash_growler, options, [ 'jquery/jquery-growler-0.1.min.js' ] )
+      
+      flash_key = flash.keys.first
+      unless [:error, :warning, :notice].include?( flash_key )
+        debugger
+        mapped_key = mappings[flash_key.to_sym]
+        flash[mapped_key.to_sym] = flash[flash_key] 
+      end
       
       if flash[:error]
         return "<div class=\"growl flash error #{position_cls}\" id=\"#{options[:id]}\"><div id=\"growl-icon-error\"></div><h3>Error</h3><p>#{flash[:error]}</p></div>"
